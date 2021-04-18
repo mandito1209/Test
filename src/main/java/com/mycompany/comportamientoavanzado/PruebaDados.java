2785554690264b6bef7efe8712bb30da7aa9a725
mandi <mandi@192.168.1.72>
2021-04-18 16:10:21 -0700

    Version 1

---
+++ src/main/java/com/mycompany/comportamientoavanzado/PruebaDados.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.comportamientoavanzado;
+
+/**
+ *
+ * @author mandi
+ */
+public class PruebaDados {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Dado d1;
+        d1=new Dado();
+        d1.randomDado();
+        d1.lanzarDado();
+        
+    }
+    
+}
