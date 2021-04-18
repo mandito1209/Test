f314658322b5197dc5d8f4b436f335bfe92db5ae
mandi <mandi@192.168.1.72>
2021-04-18 16:04:38 -0700

    Version 1

---
+++ src/main/java/com/mycompany/comportamientoavanzado/PruebaMoneda.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.comportamientoavanzado;
+
+/**
+ *
+ * @author mandi
+ */
+public class PruebaMoneda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Moneda prueba;
+        prueba= new Moneda();
+        prueba.tirar();
+        prueba.lanzarMoneda();
+    }
+    
+}
